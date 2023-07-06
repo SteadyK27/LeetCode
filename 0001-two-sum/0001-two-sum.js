2741d2346a24172f3a1a5752a7fe6252c736ed4c
HyunJoon Kang <107802867+SteadyK27@users.noreply.github.com>
2023-07-06 11:56:13 +0900

    Time: 66 ms (74.45%), Space: 43.2 MB (26.41%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -8,11 +8,8 @@ const twoSum = function (nums, target) {
     const map = new Map();
 
     for (let i = 0; i < nums.length; i += 1) {
-        if (map.has(target - nums[i])) {
-            return [map.get(target - nums[i]), i];
-        } else {
-            map.set(nums[i], i);
-        }
+        if (map.has(nums[i])) return [map.get(nums[i]), i];
+        map.set(target - nums[i], i);
     }
 
     return [];
