98d5fde0117fbec4fb1555e2e2e795b2cfae2ddd
HyunJoon Kang <107802867+SteadyK27@users.noreply.github.com>
2023-07-10 15:43:38 +0900

    Time: 66 ms (17.96%), Space: 42.2 MB (50.98%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.js
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -5,8 +5,8 @@
  */
 const searchInsert = function(nums, target) {
     // 접근 방식: O(log n) 으로 탐색하라고 했으니 이진 탐색으로 풀어야겠다.
-    // 시간 복잡도:  
-    // 공간 복잡도: 
+    // 시간 복잡도: O(log n) 
+    // 공간 복잡도: O(1)
 
     let left = 0;
     let right = nums.length - 1;
